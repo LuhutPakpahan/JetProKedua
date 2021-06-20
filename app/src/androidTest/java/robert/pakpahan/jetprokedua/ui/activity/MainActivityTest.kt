a72ee9ee08b03bfc856f5fa0baa577a9b1adcd28
LuhutPakpahan <robertus.pakpahan@students.amikom.ac.id>
2021-06-20 20:59:56 +0700

    revisi 1

--- app/src/androidTest/java/robert/pakpahan/jetprokedua/ui/activity/MainActivityTest.kt
+++ app/src/androidTest/java/robert/pakpahan/jetprokedua/ui/activity/MainActivityTest.kt
@@ -8,20 +8,14 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import org.hamcrest.CoreMatchers
 import org.junit.After
 import org.junit.Test
 import org.junit.Before
 import robert.pakpahan.jetprokedua.R
-import robert.pakpahan.jetprokedua.utils.DataDummy
 import robert.pakpahan.jetprokedua.utils.EspressoIdlingResource
 
 class MainActivityTest {
 
-    private val dummyMovie = DataDummy.getMovies()
-    private val dummyTvShow = DataDummy.getTvShows()
-    private val dummyDetailMovie = DataDummy.getDetailMovie()
-    private val dummyDetailTvShow = DataDummy.getDetailTvShow()
 
     @Before
     fun setup() {
@@ -39,7 +33,6 @@ class MainActivityTest {
         Espresso.onView(withId(R.id.rv_movies))
             .check(ViewAssertions.matches(isDisplayed()))
         Espresso.onView(withId(R.id.rv_movies))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovie.size))
     }
 
     @Test
@@ -48,15 +41,11 @@ class MainActivityTest {
             ViewActions.click()
         ))
         Espresso.onView(withId(R.id.iv_detail)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.iv_detail)).check(ViewAssertions.matches(withTagValue(CoreMatchers.equalTo(dummyDetailMovie.posterPath))))
         Espresso.onView(withId(R.id.coordinator_layout)).perform(ViewActions.swipeUp())
         Espresso.onView(withId(R.id.iv_backdrop)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.iv_backdrop)).check(ViewAssertions.matches(withTagValue(CoreMatchers.equalTo(dummyDetailMovie.backdropPath))))
         Espresso.onView(withId(R.id.collapsing)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.collapsing)).check(ViewAssertions.matches(withContentDescription(dummyDetailMovie.title)))
         Espresso.onView(withId(R.id.tv_detail_genre_duration)).check(ViewAssertions.matches(isDisplayed()))
         Espresso.onView(withId(R.id.tv_detail_overview)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.tv_detail_overview)).check(ViewAssertions.matches(withText(dummyDetailMovie.overview)))
     }
 
     @Test
@@ -65,7 +54,6 @@ class MainActivityTest {
         Espresso.onView(withId(R.id.rv_tv_shows))
             .check(ViewAssertions.matches(isDisplayed()))
         Espresso.onView(withId(R.id.rv_tv_shows))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvShow.size))
     }
 
     @Test
@@ -73,14 +61,10 @@ class MainActivityTest {
         Espresso.onView(withText("TV SHOW")).perform(ViewActions.click())
         Espresso.onView(withId(R.id.rv_tv_shows)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
         Espresso.onView(withId(R.id.iv_detail)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.iv_detail)).check(ViewAssertions.matches(withTagValue(CoreMatchers.equalTo(dummyDetailTvShow.posterPath))))
         Espresso.onView(withId(R.id.coordinator_layout)).perform(ViewActions.swipeUp())
         Espresso.onView(withId(R.id.iv_backdrop)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.iv_backdrop)).check(ViewAssertions.matches(withTagValue(CoreMatchers.equalTo(dummyDetailTvShow.backdropPath))))
         Espresso.onView(withId(R.id.collapsing)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.collapsing)).check(ViewAssertions.matches(withContentDescription(dummyDetailTvShow.title)))
         Espresso.onView(withId(R.id.tv_detail_genre_duration)).check(ViewAssertions.matches(isDisplayed()))
         Espresso.onView(withId(R.id.tv_detail_overview)).check(ViewAssertions.matches(isDisplayed()))
-        Espresso.onView(withId(R.id.tv_detail_overview)).check(ViewAssertions.matches(withText(dummyDetailTvShow.overview)))
     }
 }
